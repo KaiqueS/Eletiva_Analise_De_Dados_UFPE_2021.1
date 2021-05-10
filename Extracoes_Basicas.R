8678f3efe7613624974df4f8acede4c1ed17e606
Kaique <14958834+KaiqueS@users.noreply.github.com>
2021-05-09 22:15:22 -0300

    Exercício Extrações Básicas

---
+++ Extracoes_Basicas.R
@@ -0,0 +1,37 @@
+stall.packages( 'XML' )
+install.packages( 'rjson' )
+install.packages( 'readxl' )
+
+library( readxl )
+library( rjson )
+library( XML )
+library( rvest )
+library( dplyr )
+
+###
+
+wikipedia_csv_url <- "https://pt.wikipedia.org/wiki/Comma-separated_values"
+
+csvTables <- wikipedia_csv_url %>% read_html %>% html_nodes( "table" )
+
+csvLinks <- wikipedia_csv_url %>% read_html %>% html_nodes( "link" )
+
+wikipedia_csv <- as.data.frame( html_table( csvTables ) )
+
+###
+
+wikipedia_txt_url <- "https://pt.wikipedia.org/wiki/Arquivo_de_texto"
+
+wikipedia_txt <- read_html( wikipedia_txt_url )
+
+wikipedia_txt <- wikipedia_txt %>% html_nodes( "p" ) %>% html_text( )
+
+txtLinks <- wikipedia_txt_url %>% read_html %>% html_nodes( "link" )
+
+###
+
+wikipedia_excel_url <- "https://pt.wikipedia.org/wiki/Microsoft_Excel"
+
+wikipedia_excel <- read_html( wikipedia_txt_url )
+
+wikipedia_excel <- wikipedia_excel %>% html_nodes( "ul" )
